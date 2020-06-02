43c0e4179262584d29e9f68745fb5bff59548b3e
Jade Shenker <jsarahshenker@gmail.com>
2020-06-01 23:50:39 -0700

    small formatting edits

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import DateInfo from './DateInfo';
 
 function Header(props)
 {
+    var day = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+
+    var date = month + '.' + day + '.' + year;
 
     return(
         <div className="tasks-header">
-            <DateInfo />
+            <div className="date-wrapper">
+                <i class="far fa-clock"></i>
+                <b className="date-info">{date}</b>
+            </div>
             <h1 className="todo-card-title">today's tasks ({props.numTodos})</h1>
         </div>
     )
